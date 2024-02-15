6768ffd5357fbc42d91aa2525abcd44592ba8d43
LENEVO <akshay.garg130803@gmail.com>
2024-02-15 11:27:50 +0530

    no auth society get

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -11,13 +11,13 @@ import (
 
 var AuthenticationNotRequired map[string]bool = map[string]bool{
 	"/create": true,
+	"/soc/get":true,
 }
 
 var RoleMethods = map[string][]string{
 	"/users/get":       {utils.AdminRole, utils.SuperAdminRole},
 	"/users/update/":   {utils.AdminRole, utils.UserRole, utils.SuperAdminRole},
 	"/soc/register":    {utils.SuperAdminRole},
-	"/soc/get":         {utils.AdminRole, utils.UserRole, utils.SuperAdminRole},
 	"/soc/update/":     {utils.AdminRole, utils.SuperAdminRole},
 	"/event/create":    {utils.AdminRole, utils.SuperAdminRole},
 	"/event/update/":   {utils.AdminRole, utils.SuperAdminRole},
